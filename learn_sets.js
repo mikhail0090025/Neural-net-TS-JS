9fab84a51efd575bbf38f4f938c99a0713ca021c
mikhail0090025 <52649381+mikhail0090025@users.noreply.github.com>
2024-03-10 20:52:29 +0100

    interm commit

--- learn_sets.js
+++ learn_sets.js
@@ -10,14 +10,28 @@ db1.AddPair([0, 0, 1, 0, 0, 1, 0, 0, 1], [0, 1]); // 0 and 1 means that vertical
 db1.AddPair([1, 1, 1, 0, 0, 1, 0, 0, 1], [1, 1]); // 1 and 1 means that both horizontal and vertical line was found
 db1.AddPair([1, 1, 1, 0, 1, 0, 0, 1, 0], [1, 1]); // 1 and 1 means that both horizontal and vertical line was found
 db1.AddPair([0, 1, 0, 1, 1, 1, 0, 1, 0], [1, 1]); // 1 and 1 means that both horizontal and vertical line was found
+db1.AddPair([1, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0, 1, 0, 0, 1, 0, 0, 0, 0], [0, 0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0, 0, 1, 0, 0, 1, 0, 0, 0], [0, 0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([1, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0, 0, 0, 1, 1, 0, 0, 0, 0], [0, 0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0, 0, 0, 0, 0, 0, 1, 1, 0], [0, 0]); // 0 and 0 means that nor horizontal or vertical line was found
 var gen1 = new Generation(9, 2, 3, 10, RoundMethod.ZeroAndOne, RoundMethod.Tanh, RoundMethod.ZeroAndOne, 50, 0.001, db1);
 // SQUARE OF A NUMBER
 var db2 = new LearningDB(2, 1);
-var difference = 1000;
-for (var i = -difference; i <= difference; i++) {
-    var num = i / difference;
+var difference2 = 10;
+for (var i = -difference2; i <= difference2; i++) {
+    var num = i / difference2;
     db2.AddPair([num, num < 0 ? 1 : 0], [num * num]);
     // Input 1 - number, square we want to get
     // Input 2 - Is or not positive
 }
 var gen2 = new Generation(2, 1, 5, 20, RoundMethod.DontRound, RoundMethod.DontRound, RoundMethod.Tanh, 50, 0.001, db2);
+// Returns SQRT(input) - 0.5
+var db3 = new LearningDB(1, 1);
+var difference3 = 10;
+for (var i = 0; i <= difference3; i++) {
+    var num = i / difference3;
+    db3.AddPair([num], [Math.sqrt(num) - 0.5]);
+}
+var gen3 = new Generation(1, 1, 10, 30, RoundMethod.DontRound, RoundMethod.Tanh, RoundMethod.ZeroAndOne, 50, 0.001, db3);
--- learn_sets.ts
+++ learn_sets.ts
@@ -11,15 +11,31 @@ db1.AddPair([0,0,1,0,0,1,0,0,1], [0,1]); // 0 and 1 means that vertical line was
 db1.AddPair([1,1,1,0,0,1,0,0,1], [1,1]); // 1 and 1 means that both horizontal and vertical line was found
 db1.AddPair([1,1,1,0,1,0,0,1,0], [1,1]); // 1 and 1 means that both horizontal and vertical line was found
 db1.AddPair([0,1,0,1,1,1,0,1,0], [1,1]); // 1 and 1 means that both horizontal and vertical line was found
+
+db1.AddPair([1,0,0,1,0,0,0,0,0], [0,0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0,1,0,0,1,0,0,0,0], [0,0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0,0,1,0,0,1,0,0,0], [0,0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([1,1,0,0,0,0,0,0,0], [0,0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0,0,0,1,1,0,0,0,0], [0,0]); // 0 and 0 means that nor horizontal or vertical line was found
+db1.AddPair([0,0,0,0,0,0,1,1,0], [0,0]); // 0 and 0 means that nor horizontal or vertical line was found
 var gen1: Generation = new Generation(9, 2, 3, 10, RoundMethod.ZeroAndOne, RoundMethod.Tanh, RoundMethod.ZeroAndOne, 50, 0.001, db1);
 
 // SQUARE OF A NUMBER
 var db2: LearningDB = new LearningDB(2, 1);
-let difference = 1000;
-for (let i = -difference; i <= difference; i++) {
-    let num = i / difference;
+let difference2 = 10;
+for (let i = -difference2; i <= difference2; i++) {
+    let num = i / difference2;
     db2.AddPair([num, num < 0 ? 1 : 0], [num * num]);
     // Input 1 - number, square we want to get
     // Input 2 - Is or not positive
 }
 var gen2: Generation = new Generation(2,1,5,20,RoundMethod.DontRound, RoundMethod.DontRound,RoundMethod.Tanh, 50, 0.001, db2);
+
+// Returns SQRT(input) - 0.5
+var db3: LearningDB = new LearningDB(1, 1);
+let difference3 = 10;
+for (let i = 0; i <= difference3; i++) {
+    let num = i / difference3;
+    db3.AddPair([num], [Math.sqrt(num) - 0.5]);
+}
+var gen3: Generation = new Generation(1,1,10,30,RoundMethod.DontRound, RoundMethod.Tanh,RoundMethod.ZeroAndOne, 50, 0.001, db3);
